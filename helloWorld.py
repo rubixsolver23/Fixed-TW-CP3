17acf0f9c61b4c8eef4ae980c9fa02ae901dc93b
rubixsolver23 <tylerclaywatts@gmail.com>
2024-09-10 11:42:01 -0600

    fixed it

--- helloWorld.py
+++ helloWorld.py
@@ -3,12 +3,13 @@ import random
 
 print("This will be a completely new project now!!!")
 
+time.sleep(5)
 while True:
-    time.sleep(random.random * 5)
-    time1 = time.time
+    time.sleep(random.random() * 5)
+    time1 = time.time()
     timerr = input("Now!!!")
-    total = time.time - time1
-    print("It took you %s seconds bro")
+    total = time.time() - time1
+    print("It took you %s seconds bro"% total)
     if total > 3:
         break
 print("You lose, too slow")
