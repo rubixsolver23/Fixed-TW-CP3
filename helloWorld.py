28bdef025a17e9c673388fccc17d19bcd5ac7848
rubixsolver23 <135743528+rubixsolver23@users.noreply.github.com>
2024-09-10 17:46:03 +0000

    made more fun

--- helloWorld.py
+++ helloWorld.py
@@ -7,9 +7,13 @@ time.sleep(5)
 while True:
     time.sleep(random.random() * 5)
     time1 = time.time()
+    choice = random.choice(["type", "yes"])
+    print(choice)
     timerr = input("Now!!!")
+    if timerr != choice:
+        break
     total = time.time() - time1
     print("It took you %s seconds bro"% total)
     if total > 3:
         break
-print("You lose, too slow")
+print("You lose LLLLLL")
