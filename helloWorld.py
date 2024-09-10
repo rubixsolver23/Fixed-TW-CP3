a3a5b397cc3219dec6b2a97593506f7c0d01ea12
rubixsolver23 <tylerclaywatts@gmail.com>
2024-09-10 11:48:02 -0600

    final change

--- helloWorld.py
+++ helloWorld.py
@@ -1,19 +1,19 @@
 import time
 import random
 
-print("This will be a completely new project now!!!")
+print("Type one of the following or die in the lavaAAA")
 
 time.sleep(5)
 while True:
     time.sleep(random.random() * 5)
     time1 = time.time()
-    choice = random.choice(["type", "yes"])
+    choice = random.choice(["left", "right", "up", "down", "jump", "lean"])
     print(choice)
     timerr = input("Now!!!")
     if timerr != choice:
         break
     total = time.time() - time1
     print("It took you %s seconds bro"% total)
-    if total > 3:
+    if total > 4:
         break
 print("You lose LLLLLL")
