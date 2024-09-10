02ca53962ad5fc7829ef8cd87488b7830d4c535c
rubixsolver23 <135743528+rubixsolver23@users.noreply.github.com>
2024-09-10 17:37:42 +0000

    made a minigame

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,14 @@
+import time
+import random
+
 print("This will be a completely new project now!!!")
+
+while True:
+    time.sleep(random.random * 5)
+    time1 = time.time
+    timerr = input("Now!!!")
+    total = time.time - time1
+    print("It took you %s seconds bro")
+    if total > 3:
+        break
+print("You lose, too slow")
