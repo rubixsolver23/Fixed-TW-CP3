5b965c29dde9e2abaf667b8d22287c998b6f616b
rubixsolver23 <tylerclaywatts@gmail.com>
2024-10-24 11:49:08 -0600

    JumpyBoi updates

--- Platformer with fun stuff/JumpyBoiUNIVERSE.py
+++ Platformer with fun stuff/JumpyBoiUNIVERSE.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import os, sys
 
 mainClock = pygame.time.Clock()
@@ -23,29 +24,46 @@ class LevelManager:
     pass
 
 class Level:
-    def __init__(self, id, size, block_list):
+    def __init__(self, id, level_dict, block_size):
         self.id = id
-        self.size = size
-        self.block_list = block_list
+        self.level_dict = level_dict
+        self.block_object_list = []
+        self.block_size = block_size
+
+        self.create_block_objects()
+
+    def create_block_objects(self):
+        width = self.level_dict["width"]
+        height = self.level_dict["height"]
+        for idx, block in enumerate(self.level_dict["blocklist"]):
+            if block == "B":
+                block_color = (0,0,0)
+                block_hitbox = pygame.Rect(0, 0, self.block_size, self.block_size)
+                self.block_object_list.append(RegBlock(idx%width, idx//width, block_color, block_hitbox, self.block_size))
 
 class LevelEditor:
     pass
 
 
 class Block:
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color, hitbox, blocksize):
         self.x = x
         self.y = y
         self.color = color
+        self.hitbox = hitbox
+        self.blocksize = blocksize
 
-    def render(self):
-        pass
+    def render(self, camera_pos):
+        self.hitbox.centerx = self.x*self.blocksize - camera_pos[0]
+        self.hitbox.centery = self.y*self.blocksize - camera_pos[1]
+        pygame.draw.rect(windowSurface, self.color, self.hitbox)
 
 class PlayerBlock(Block):
     pass
 
 class RegBlock(Block):
-    pass
+    def __init__(self, x, y, color, hitbox, blocksize):
+        super().__init__(x, y, color, hitbox, blocksize)
 
 class LaserBlock(Block):
     pass
@@ -59,11 +77,23 @@ class WaterBlock(Block):
 class ExitBlock(Block):
     pass
 
+class Camera():
+    pass
+
 def main():
 
-    level1 = Level(1, [20, 20], {})
+    level1 = Level(1, {
+        "width": 3,
+        "height": 3,
+        "blocklist": [
+            "B", " ", "B",
+            " ", "B", " ",
+            "B", " ", "B"
+        ]
+    }, 30)
     while True:
         # FIRST
+        '''
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -72,13 +102,16 @@ def main():
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+        '''
         # then other stuff
 
-
+        windowSurface.fill((255,255,255))
+        for block in level1.block_object_list:
+            block.render([0, 0])
 
         # LAST
         pygame.display.update()
         mainClock.tick(60)
         
 
-
+main()
