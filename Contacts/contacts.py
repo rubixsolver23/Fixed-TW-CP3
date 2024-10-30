db65219f47dbf6703971d5c835216058abc74217
rubixsolver23 <tylerclaywatts@gmail.com>
2024-10-30 11:23:23 -0600

    Finished contacts assignment

--- Contacts/contacts.py
+++ Contacts/contacts.py
@@ -27,11 +27,15 @@ class Contacts:
         if len(self.contact_list) == 0:
             self.choice = input('(A)dd a new contact \n(Q)uit \n> ').lower()
         else:
-            self.view = 'quit'
+            for index, contact in enumerate(self.contact_list):
+                print(f"{index + 1}) {contact.first_name} {contact.last_name}")
+            self.choice = input("\n(#) Select a name \n(A)dd a new contact\n(Q)uit \n>").lower()
         self.handle_choice()
 
     def show_info(self):
-        pass
+        self.contact_list[self.index].display_info()
+        self.choice = input('\n(C)ontact List \n(P)revious contact \n(N)ext contact \n(Q)uit \n> ').lower()
+        self.handle_choice()
 
     def __add__(self, new_contact):
         self.contact_list.append(new_contact)
@@ -45,6 +49,17 @@ class Contacts:
             self.view = 'quit'
         elif self.choice == 'a' and self.view == 'list':
             self.view = 'add'
+        elif self.choice.isnumeric() and self.view == "list":
+            index = int(self.choice) - 1
+            if index >= 0 and index < len(self.contact_list):
+                self.index = index
+                self.view = "info"
+        elif self.choice == "c" and self.view == "info":
+            self.view = "list"
+        elif self.choice == 'n' and self.view == 'info':
+            self.index = self.index + 1 if self.index + 1 < len(self.contact_list) else 0
+        elif self.choice == 'p' and self.view == 'info':
+            self.index = self.index - 1 if self.index - 1 >= 0 else len(self.contact_list) - 1
 
 class Information:
     def __init__(self):
@@ -55,6 +70,14 @@ class Information:
         self.work_phone = input('Enter their work phone number: ')
         self.work_email = input('Enter their work email address: ')
         self.title = input('Enter their work title: ')
+
+    def display_info(self):
+        print(f'\n{self.first_name} {self.last_name}')
+        print(f'Personal phone number: {self.personal_phone}')
+        print(f'Personal email address: {self.personal_email}')
+        print(f'Work title: {self.title}')
+        print(f'Work phone number: {self.work_phone}')
+        print(f'Work email address: {self.work_email}')
 
 contacts = Contacts()
 contacts.display()
